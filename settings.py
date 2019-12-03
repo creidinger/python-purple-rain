d572b4913b4775df3df6900caa40da3a94de07e4
Chase Reidinger <chase.reidinger@ogilvy.com>
2019-12-03 09:58:28 -0500

    ignore .pyc and adjust settings

--- settings.py
+++ settings.py
@@ -12,6 +12,8 @@ class Settings():
         self.rain_height = 20
         self.rain_width = 7
         self.rain_speed_factor = 20
-        self.rain_speed_factor_min = self.rain_speed_factor / 2
+        # set the min fall speed to 75%
+        # so the background doesn't fall to slowly
+        self.rain_speed_factor_min = self.rain_speed_factor * 0.75
         self.rain_color = (85, 0, 200)
         self.number_raindrops = 800  # the nubmer of raindrops on the screen
